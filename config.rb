dc42dc919a060b89556e65520da81221ba62c1f4
Ben Bettridge <bbettridge@switch.tv>
2015-06-25 14:02:48 +1000

    Refactored for Passenger Support.

--- config.rb
+++ config.rb
@@ -1,4 +1,4 @@
-UPSTREAM_KIBANA_HOST = "kibana.example.com"
-UPSTREAM_ELASTICSEARCH_HOST = "elasticsearch.example.com"
+UPSTREAM_KIBANA_HOST = "intpkibana01.prod.switch.internal:81"
+UPSTREAM_ELASTICSEARCH_HOST = "intpelasticslb01.prod.switch.internal"
 UPSTREAM_ELASTICSEARCH_PORT = 9200
 KIBANA_INDEX = ".kibana4"
